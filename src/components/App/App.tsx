9f88ddca53d71ce073e8bd813fab5a1c4a7ee589
Olecsii <vyriy3050@gmail.com>
2025-07-02 00:14:58 +0300

    02-react-cafe

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
-import Notification from "../Notfication/Notification";
+import Notification from "../Notification/Notification";
 import css from "./App.module.css";
 import type { Votes, VoteType } from "../../types/votes";
 
